e92de48898b2815933b48c4fa2175b6f1c651cb2
Jorge Dotel <jorgedotel@gmail.com>
2026-09-10 17:53:11 -0400

    Una lista con nombre

--- fundamentos_ts/src/main.ts
+++ fundamentos_ts/src/main.ts
@@ -1,17 +1,12 @@
-//Elegir caminos con if, else if y else
-//La cadena que sale bien
-function describirPrioridad(nivel: number): string {
-  if (nivel >= 8) {
-    return "alta";
-  } else if (nivel >= 4) {
-    return "media";
-  } else {
-    return "baja";
-  }
-}
+//Consultar arreglos mediante índices, longitud y casos vacíos
+//Una lista con nombres
+const titulos: string[] = [
+  "Estructurar HTML",
+  "Aplicar CSS",
+  "Practicar TypeScript",
+];
 
-console.log(describirPrioridad(9));
-console.log(describirPrioridad(8));
-console.log(describirPrioridad(4));
-console.log(describirPrioridad(3));
-console.log(describirPrioridad(0));
+//recorrer el arreglo con un for
+for (const titulo of titulos) {
+  console.log(titulo);
+}
