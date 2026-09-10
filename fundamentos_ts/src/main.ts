f1df008a9d412983768dc4e337c1f199d7e80883
Jorge Dotel <jorgedotel@gmail.com>
2026-09-10 17:30:15 -0400

    Dos funciones trabajan juntas

--- fundamentos_ts/src/main.ts
+++ fundamentos_ts/src/main.ts
@@ -1,6 +1,18 @@
+//Dos funciones que trabajan juntas
 function calcularPendientes(total: number, completadas: number): number {
   return total - completadas;
 }
 
-const pendientes = calcularPendientes(8, 3);
+function calcularPorcentaje(parte: number, total: number): number {
+  return (parte / total) * 100;
+}
+
+const total = 8;
+const completadas = 3;
+
+const pendientes = calcularPendientes(total, completadas);
+const porcentaje = calcularPorcentaje(pendientes, total);
+
 console.log(pendientes);
+console.log(porcentaje.toFixed(1));
+console.log(calcularPendientes(2, 2));
